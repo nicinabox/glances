d7e06703ffeca3b2ef72704cfc57a4576815e3c5
Nic Aitch <nic@nicinabox.com>
2016-01-18 16:46:01 -0600

    Add connection status

--- app/javascripts/components/App.js
+++ app/javascripts/components/App.js
@@ -7,24 +7,54 @@ var React = require('react')
 var Tile = require('./Tile')
 var socket = io()
 
+const CONNECTED = 'connected'
+const CONNECTING = 'connecting'
+const RECONNECTING = 'reconnecting'
+const DISCONNECTED = 'disconnected'
+
 module.exports = React.createClass({
   getInitialState() {
     return {
-      tiles: []
+      tiles: [],
+      status: CONNECTING
     }
   },
 
   componentDidMount() {
+    socket.on('reconnect_failed', () => {
+      this.setState({ status: DISCONNECTED })
+    })
+
+    socket.on('connect', () => {
+      this.setState({ status: CONNECTED })
+    })
+
+    socket.on('reconnect', () => {
+      this.setState({ status: CONNECTED })
+    })
+
+    socket.on('reconnecting', () => {
+      this.setState({ status: RECONNECTING })
+    })
+
     socket.on('tiles', (tiles) => {
-      tiles = reject(tiles, 'disabled')
       this.setState({ tiles })
     })
   },
 
   render() {
+    var statusClassNames = ['status', `status-${this.state.status}`].join(' ')
+
     return (
-      <div className="container">
-        {this.state.tiles.map((props, i) => <Tile key={`tile-${i}`} {...props} />)}
+      <div>
+        <div className={statusClassNames}>
+          <span className="indicator"></span>
+          {this.state.status}
+        </div>
+
+        <div className="container">
+          {this.state.tiles.map((props, i) => <Tile key={`tile-${i}`} {...props} />)}
+        </div>
       </div>
     )
   }
