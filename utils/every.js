36495425e749ebd5f91c14928bc3fec597b8f499
Nic Aitch <nic@nicinabox.com>
2016-01-17 23:23:54 -0600

    Refactor 'every' util

--- utils/every.js
+++ utils/every.js
@@ -1,3 +1,7 @@
+var path = require('path')
+var stack = require('callsite')
+var logger = require('./logger')
+
 var toMs = function (str) {
   var units = {
     second: 1000,
@@ -6,30 +10,40 @@ var toMs = function (str) {
     day: 1000 * 60 * 60 * 24,
   }
 
-  var matches = str.match(/(\d+)\s(\w+)/)
-  var interval = matches[1]
-  var unit = matches[2]
+  var matches = str.match(/(\d+)\s?(\w+)/)
+  var interval = +matches[1]
 
-  return (units[unit] || units[unit.replace(/s$/, '')]) * +interval
+  var unit = Object.keys(units).filter((u) => {
+    var re = new RegExp(`^${matches[2]}?`)
+    return re.test(u)
+  })[0]
+
+  var duration = units[unit]
+
+  return duration * interval
 }
 
-module.exports = function (intervalStr, fn) {
-  var interval = toMs(intervalStr)
-
-  var done = function () {
-    setTimeout(callFn, interval)
+module.exports = function (intervalStr, desc, fn) {
+  if (typeof desc == 'function') {
+    fn = desc
+    desc = ''
   }
 
-  var callFn = function () {
+
+  var caller = desc || path.basename(stack()[1].getFileName())
+  var interval = toMs(intervalStr)
+  var done = () => setTimeout(callFn, interval)
+
+  if (!interval) {
+    console.log('=>', 'No interval for', intervalStr)
+  }
+
+  var callFn = () => {
+    logger.log('Every', intervalStr, caller)
+
     var p = fn(done)
     if (p instanceof Promise) p.then(done)
   }
 
-  fn(done)
-
-  if (interval) {
-    setTimeout(callFn, interval)
-  } else {
-    console.log('=>', 'No interval for', intervalStr)
-  }
+  callFn()
 }
--- utils/ping.js
+++ utils/ping.js
@@ -3,8 +3,6 @@ var logger = require('./logger')
 
 module.exports = function (target) {
   return new Promise(function (resolve, reject) {
-    logger.log('Ping', target)
-
     exec(`ping -c 1 ${target}`, function (err, stdout, stderr) {
       var isReceived = (/packets received/).test(stdout)
 
