865eb1cdf0a00c9b141d69ffe97a5628f7d1a7cb
Nic Aitch <nic@nicinabox.com>
2016-01-21 23:21:04 -0600

    Improve error handling in decorateState

--- app/lib/decorateState.js
+++ app/lib/decorateState.js
@@ -1,4 +1,8 @@
 module.exports = function (state) {
+  if (!(state.id || state.title)) {
+    throw new Error(`Tile state must include title (${JSON.stringify(state)})`)
+  }
+
   return Object.assign({}, state, {
     id: state.id || state.title.toLowerCase().replace(/\s/g, '-'),
     updatedAt: new Date()
