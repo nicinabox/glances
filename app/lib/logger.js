d6128def9dabf0919d68030bfe365f3e7c658100
Nic Aitch <nic@nicinabox.com>
2016-01-23 12:58:28 -0600

    Refactor logger for improved output

--- app/lib/logger.js
+++ app/lib/logger.js
@@ -1,24 +1,26 @@
-var moment = require('moment')
-
-var timestamp = function () {
-  return moment().format('D MMM YYYY HH:mm:ss')
-}
+var chalk = require('chalk')
+var toArray = require('lodash/toArray')
 
 var makeLine = function (prefix, args) {
-  return [
-    '[' + timestamp() + ']',
-    prefix
-  ].concat(Array.prototype.slice.call(args))
+  return [prefix].concat(args)
 }
 
 module.exports = {
   log: function () {
     if (process.env.NODE_ENV === 'test') return
-    console.log.apply(console, makeLine('=>', arguments))
+
+    var args = toArray(arguments)
+    console.log.apply(console, makeLine(chalk.green('-->'), args))
   },
 
   error: function () {
     if (process.env.NODE_ENV === 'test') return
-    console.warn.apply(console, makeLine('!!', arguments))
+    var args = arguments
+
+    if (args[0] instanceof Error) {
+      args = args[0].message
+    }
+
+    console.log.apply(console, makeLine(chalk.red('!!!'), chalk.red(args)))
   }
 }
